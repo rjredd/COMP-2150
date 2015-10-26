80abc936f61e8919c8ba70f75dfec99e35c54df1
rjredd <rjredd@memphis.edu>
2015-10-25 22:40:57 -0500

    Updated labs 6 and 7 to complete.

--- lab6/GenericArrayList.java
+++ lab6/GenericArrayList.java
@@ -91,21 +91,186 @@ public class GenericArrayList<E> implements List<E>
         data[index] = value;
 
     }
-    
+
+    /**
+     * Isn't this just a getter method for size?
+     * 
+     * @author Riley Redd
+     * @return The number of non-null elements in the arraylist.
+     */
+    public int size()
+    {
+        return this.size;
+    }
+
+    /**
+     * @author Riley Redd
+     * @param anotherList
+     *            a Generic ArrayList of the same generic type.
+     */
+    public void addAll(GenericArrayList<E> anotherList)
+    {
+        for (int i = 0; i < anotherList.size; i++)
+        {
+            if (anotherList.data[i] != null)
+                this.add(anotherList.data[i]);
+        }
+    }
+
+    /**
+     * @author Riley Redd
+     * @param someData
+     *            an object of the same generic type as the calling object
+     * @return whether or not the calling array contains the requested data.
+     */
+    public boolean contains(E someData)
+    {
+        for (int i = 0; i < this.size(); i++)
+        {
+            if (this.data[i].equals(someData))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * SPAGHETTI COOOOODE XD
+     * 
+     * @author Riley Redd
+     * @param anotherList
+     *            a Generic ArrayList of the same generic type.
+     */
+    public void retainAll(GenericArrayList<E> anotherList)
+    {
+        // Spaghetti code part 2
+        escape: for (int i = this.size() - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < anotherList.size(); j++)
+            {
+                if (this.data[i] == anotherList.data[j])
+                {
+                    // Spaghetti code part 1
+                    continue escape;
+                }
+            }
+            this.remove(i);
+        }
+    }
+
+    /**
+     * @author Riley Redd
+     * @param index
+     *            the index of the array that you wish to remove.
+     */
+    public void removeAndTrim(int index)
+    {
+        this.remove(index);
+
+        if (this.size() < (data.length / 2))
+        {
+            System.out.println("Resizing array to capacity " + data.length / 2);
+            E[] newArray = (E[]) new Object[data.length / 2];
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = data[i];
+            }
+            data = newArray;
+        }
+    }
+
+    /**
+     * @author Riley Redd
+     * @param beginIndex
+     *            an int that should greater than zero and less than endIndex.
+     * @param endIndex
+     *            an int that should be greater than beginIndex and less that
+     *            arrayList size.
+     * @return a new GenericArrayList<E> that is made up of the values of the
+     *         calling objects data between said indexes.
+     */
+    public GenericArrayList<E> subList(int beginIndex, int endIndex)
+    {
+        if (beginIndex < 0 || beginIndex > this.size() || endIndex < 0
+                || endIndex > this.size() || beginIndex > endIndex)
+        {
+            return null;
+        }
+
+        GenericArrayList<E> temp = new GenericArrayList<E>();
+
+        if (endIndex - beginIndex > 0)
+        {
+            for (int i = beginIndex; i < endIndex; i++)
+            {
+                temp.add(this.get(i));
+            }
+        }
+
+        return temp;
+    }
+
     public static void main(String[] args)
     {
         GenericArrayList<Integer> myList = new GenericArrayList<Integer>();
+        GenericArrayList<Integer> aList = new GenericArrayList<Integer>();
         for (int i = 0; i < 80; i++)
         {
             myList.add(i);
         }
-        myList.remove(0);
-        myList.set(23, 4444);
-        for (int i = 0; i < 79; i++)
+
+        aList.add(1);
+        aList.add(3);
+        aList.add(20);
+        aList.add(67);
+
+        System.out.println(myList.size());
+
+        myList.addAll(aList);
+
+        // show that i have added to lists.
+        for (int i = 0; i < myList.size(); i++)
         {
             System.out.println(myList.get(i));
         }
 
+        // testing subList method
+        GenericArrayList<Integer> subListA = myList.subList(1, 5);
+        for (int i = 0; i < subListA.size(); i++)
+        {
+            System.out.println(subListA.get(i));
+        }
+
+        // testing the removeAndTrim method.
+        for (int i = myList.size() - 1; i >= 20; i--)
+        {
+            myList.removeAndTrim(i);
+        }
+        for (int i = 0; i < myList.size(); i++)
+        {
+            System.out.println(myList.get(i));
+        }
+
+        // does myList contain the number 1?
+        System.out.println(myList.contains(1));
+
+        // does myList contain the number 81?
+        System.out.println(myList.contains(81));
+
+        // change myList and print it out.
+        myList.retainAll(aList);
+        for (int i = 0; i < myList.size(); i++)
+        {
+            System.out.println(myList.get(i));
+        }
+
+        for (int i = myList.size() - 1; i >= 20; i--)
+        {
+            myList.remove(i);
+        }
+        for (int i = 0; i < myList.size(); i++)
+        {
+            System.out.println(myList.get(i));
+        }
     }
 
 }
--- lab7/GenericLinkedList.java
+++ lab7/GenericLinkedList.java
@@ -1,5 +1,10 @@
 package lab7;
 
+/**
+ * @author Ernest McCracken, Riley Redd
+ * @param <E>
+ *            a generic variable
+ */
 public class GenericLinkedList<E> implements List<E>
 {
 
@@ -12,6 +17,11 @@ public class GenericLinkedList<E> implements List<E>
     private Node<E> head = null;
     private int size = 0;
 
+    /**
+     * @author Ernest McCracken
+     * @param index
+     * @return
+     */
     private Node<E> nodeAt(int index)
     {
 
@@ -48,6 +58,11 @@ public class GenericLinkedList<E> implements List<E>
         size++;
     }
 
+    /**
+     * @author Ernest McCracken
+     * @param index
+     * @param value
+     */
     public void add(int index, E value)
     {
         Node<E> node = new Node<E>();
@@ -90,38 +105,209 @@ public class GenericLinkedList<E> implements List<E>
 
     }
 
+    /*
+     * Had to edit this method in order for Null lists to work.
+     */
+    @Override
     public String toString()
     {
         String s = "";
         Node<E> curNode = head;
-        s += curNode.data + " -> ";
-        while (curNode.next != null)
+        if (curNode != null) // added null check here. RR.
         {
-            curNode = curNode.next;
             s += curNode.data + " -> ";
+            while (curNode.next != null)
+            {
+                curNode = curNode.next;
+                s += curNode.data + " -> ";
+            }
         }
         s += "null";
         return s;
     }
 
+    /**
+     * @author Riley Redd
+     */
+    public void clear()
+    {
+        // simple and clean. No list exists with these settings.
+        this.head = null;
+        this.size = 0;
+    }
+
+    /**
+     * @author Riley Redd
+     * @param o
+     *            the Object your are comparing to of type E.
+     * @return the amount of like objects in the list.
+     */
+    public int count(E o)
+    {
+        int countNum = 0;
+
+        for (int i = 0; i < this.size; i++)
+            if (o.equals(this.get(i)))
+                countNum++;
+
+        return countNum;
+    }
+
+    /**
+     * @author Riley Redd
+     */
+    public void reverse()
+    {
+        GenericLinkedList<E> tempList = new GenericLinkedList<E>();
+
+        // setup reverse list.
+        for (int i = size - 1; i >= 0; i--)
+        {
+            tempList.add(this.get(i));
+        }
+
+        // clear current list
+        this.clear();
+
+        // set current list to have nodes equivalent to reversed list's nodes.
+        for (int i = 0; i < tempList.size; i++)
+            this.add(tempList.get(i));
+    }
+
+    /**
+     * @author Riley Redd
+     * @param o
+     */
+    public void removeAll(E o)
+    {
+        for (int i = 0; i < this.size; i++)
+        {
+            if (o.equals(this.get(i)))
+            {
+                this.remove(i);
+                i--; // reset i one step.
+                size--; // shrink entire list by 1.
+            }
+        }
+    }
+
+    /**
+     * @author Riley Redd
+     * @param beginIndex
+     *            the node you want the new list to start at.
+     * @param endIndex
+     *            the node after the one you want last in your new list.
+     * @return a "sublisted" list.
+     */
+    public GenericLinkedList<E> subList(int beginIndex, int endIndex)
+    {
+        GenericLinkedList<E> subList = new GenericLinkedList<E>();
+
+        if (beginIndex < 0 || endIndex > this.size || beginIndex > endIndex)
+            return null; // invalid index
+        if (beginIndex == endIndex)
+            return subList; // a single node cannot be exclusive and inclusive.
+
+        for (int i = beginIndex; i < endIndex; i++)
+        {
+            subList.add(this.get(i));
+        }
+
+        return subList;
+
+    }
+
     public static void main(String[] args)
     {
-        List<Integer> myList = new GenericLinkedList<Integer>();
-        myList.add(4);
-        myList.add(7);
-        myList.add(123123);
 
-        System.out.println(myList.get(2));
+        // List<Integer> myList = new GenericLinkedList<Integer>();
+        // myList.add(4);
+        // myList.add(7);
+        // myList.add(123123);
+        //
+        // System.out.println(myList.get(2));
+        //
+        // GenericLinkedList<Integer> myList2 = new
+        // GenericLinkedList<Integer>();
+        // myList2.add(4);
+        // myList2.add(7);
+        // myList2.add(8);
+        // myList2.add(3, 999);
+        // System.out.println(myList2);
+        //
+        // myList2.remove(3);
+        // System.out.println(myList2);
 
-        GenericLinkedList<Integer> myList2 = new GenericLinkedList<Integer>();
-        myList2.add(4);
-        myList2.add(7);
-        myList2.add(8);
-        myList2.add(3, 999);
-        System.out.println(myList2);
+        // Basic setup
+        GenericLinkedList<Integer> testList = new GenericLinkedList<Integer>();
+        testList.add(0);
+        testList.add(1);
+        testList.add(2);
+        testList.add(3);
+        testList.add(4);
+        System.out.println(testList);
 
-        myList2.remove(3);
-        System.out.println(myList2);
+        // clear() test
+        testList.clear();
+        System.out.println(testList);
+
+        // count(E o) test
+        testList.add(0);
+        testList.add(1);
+        testList.add(2);
+        testList.add(3);
+        testList.add(4);
+        System.out.println(testList.count(new Integer(3)));
+
+        // reverse() test
+        testList.reverse();
+        System.out.println(testList);
+
+        // removeAll(E o) test A
+        testList.add(2);
+        testList.add(3);
+        testList.add(2);
+        testList.add(3);
+        testList.add(2);
+        testList.add(3);
+        testList.add(2);
+        testList.add(3);
+        testList.removeAll(3);
+        System.out.println(testList);
+
+        // removeAll(E o) test B
+        testList.add(4);
+        testList.removeAll(4);
+        System.out.println(testList);
+
+        // removeAll(E o) test C
+        testList.add(3);
+        testList.add(3);
+        testList.add(3);
+        testList.add(3);
+        testList.add(3);
+        testList.add(3);
+        testList.add(2);
+        testList.removeAll(3);
+        System.out.println(testList);
+
+        // removeAll(E o) test D
+        testList.clear();
+        testList.removeAll(0);
+        System.out.println(testList);
+
+        // removeAll(E o) test E
+        testList.add(2);
+        testList.removeAll(2);
+        System.out.println(testList);
+
+        // subList(int beginIndex, int endIndex) test
+        testList.add(0);
+        testList.add(1);
+        testList.add(2);
+        testList.add(3);
+        testList.add(4);
+        System.out.println(testList.subList(5, 5));
 
     }
 
